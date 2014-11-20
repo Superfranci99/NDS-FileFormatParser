cfcad54a2475b2fd5331ef1a7eea40954c42464c
Superfranci99 <superfranci99@gmail.com>
2014-11-20 15:51:22 +0100

    Added XML and binary test files in the project
    In the XML file there are some syntax rules for how to write a correct
    XML file

--- NDS-FileFormatParser/NDS-FileFormatParser/Program.cs
+++ NDS-FileFormatParser/NDS-FileFormatParser/Program.cs
@@ -11,14 +11,24 @@ namespace NDS_FileFormatParser
         static void Main(string[] args)
         {
             Console.WriteLine("~~ NDS-FileFormatParser ~~");
-            Console.WriteLine("by Superfranci99 -- V:{0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("by Superfranci99 -- V:{0}",
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
 
             RunCommand(args);
         }
 
         static void RunCommand(string[] args)
         {
+            /*
+             * At the moment I don't really know which commands
+             * there will be and which name I will give to them
+             * For now just use this method for parse a file
+             * where args[0] = path of the file to parse
+             *       args[1] = path of the XML file
+            */
 
+            args[0] = @"Files\palette.nclr";
+            args[1] = @"Files\test.xml";
         }
     }
 }
