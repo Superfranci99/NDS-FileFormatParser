1bf5e065162fb12fd2c5c7c0dacfa69def1a260f
Superfranci99 <superfranci99@gmail.com>
2015-01-20 19:26:00 +0100

    Evaluate expressions in offset and size attributes

--- NDS-FileFormatParser/NDS-FileFormatParser/Debugging/Debugger.cs
+++ NDS-FileFormatParser/NDS-FileFormatParser/Debugging/Debugger.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NDS_FileFormatParser.XML;
 using NDS_FileFormatParser.Parsing;
+using System.Data;
 
 namespace NDS_FileFormatParser.Debugging
 {
@@ -41,11 +42,13 @@ namespace NDS_FileFormatParser.Debugging
         private void ExecutePrimitive(Xinstruction xinstruction)
         {
             // <primitive name="" type="" offset="" size="" />
+            DataTable dt = new DataTable();
+
             string name   = xinstruction.Args["name"];
             string type   = xinstruction.Args["type"];
-            int    offset = int.Parse(xinstruction.Args["offset"]);
-            int    size   = int.Parse(xinstruction.Args["size"]);
-
+            int    offset = int.Parse(dt.Compute(xinstruction.Args["offset"], "").ToString());
+            int    size   = int.Parse(dt.Compute(xinstruction.Args["size"], "").ToString());
+           
             object value = this.Parse.Read(type, offset, size);
             Console.WriteLine("{0}, {1}, {2}, {3} --> {4}", name, type, offset, size, value);
         }
